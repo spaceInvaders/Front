adceeb6b796dc4f42c5415dbee16eb6da0d6d28e
Sviatoslav <sviat.gotsko@gmail.com>
2022-04-02 18:28:47 +0300

    Added functions

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,78 @@
 "use strict";
 
-const numberOfFilms = +prompt("Hove many movies have u seen", 10);
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-for (let index = 0; index < 2; index++) {
-    const lastMovieYouSeen = prompt("Last movie you have seen", "mm...Harry Potter?");
-    if (lastMovieYouSeen == null || lastMovieYouSeen.length <= 0 || lastMovieYouSeen.length > 50) {
+start();
+rememberMyFilms();
+detectPersonalLevel();
+writeYourGenres();
+showMyDB();
+
+function start() {
+    let numberOfFilms;
+    console.log(`numberOfFilms - ${numberOfFilms}`);
+
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Hove many movies have u seen", "");
+    }
+
+    personalMovieDB.count = numberOfFilms;
+}
+
+function rememberMyFilms() {
+    for (let index = 0; index < 2; index++) {
+        const lastMovieYouSeen = prompt("Last movie you have seen", "mm...Harry Potter?");
+        if (isValidMovieOrGenre(lastMovieYouSeen)) {
+            personalMovieDB.movies[lastMovieYouSeen] = prompt(`Please rate ${lastMovieYouSeen}`, "100");
+
+            continue;
+        }
         console.log("lastMovieYouSeen must be not empty and no more than 50 chars");
         index--;
-        break;
     }
-    const lastMovieRate = prompt(`Please rate ${lastMovieYouSeen}`, "100");
-    personalMovieDB.movies[lastMovieYouSeen] = lastMovieRate;
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("personalMovieDB < 10");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("personalMovieDB >= 10 && personalMovieDB < 30");
-} else if (personalMovieDB.count >= 30) {
-    console.log("personalMovieDB >= 30");
-} else {
-    console.log("Oops!");
+function isValidMovieOrGenre(input) {
+    if (input != null && input.length > 0 && input.length < 50) {
+        console.log("***true");
+        return true;
+    }
 }
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+
+    console.log("detectPersonalLevel");
+
+    if (personalMovieDB.count < 10) {
+        console.log("personalMovieDB < 10");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("personalMovieDB >= 10 && personalMovieDB < 30");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("personalMovieDB >= 30");
+    } else {
+        console.log("Oops!");
+    }
+}
+
+function writeYourGenres() {
+    for (let index = 0; index < 3; index++) {
+        const favouriteGenre = prompt(`Your favourite genre No ${index + 1}`, '');
+        if (isValidMovieOrGenre(favouriteGenre)) {
+            personalMovieDB.genres[index] = favouriteGenre;
+
+            continue;
+        } 
+        console.log("favouriteGenre must be not empty and no more than 50 chars");
+        index--;
+    }
+}
+
+function showMyDB() {
+    personalMovieDB.privat == false ? console.log(personalMovieDB) : console.log("personalMovieDB is privat");
+}
---
+++ JS/testScript.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// function showFirstMesage() {
+//     console.log("func");
+// }
+
+// showFirstMesage();
+
+// let b = () => console.log();
+
+let a = 'fd';
+
+a = a.toUpperCase();
+
+console.log(a);
