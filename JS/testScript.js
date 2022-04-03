53577a6d7b89727af96d7b74c02ed3fa1861f077
Sviatoslav <sviat.gotsko@gmail.com>
2022-04-03 21:49:26 +0300

    Added css

--- JS/testScript.js
+++ JS/testScript.js
@@ -1,48 +1,23 @@
 "use strict";
 
-const foo = {
-    a: 8,
-    b: 10,
-    c: func1
-};
+// const box = document.getElementById('box');
+// console.log(box);
 
-function func1(params) {
-    console.log(params);
-}
+const firstButton = document.getElementsByTagName('button')[0];
+firstButton.style.background = 'purple';
 
-foo.c('func1 - lala');
+console.log(firstButton);
+console.dir(firstButton);
 
-// const fee = Object.assign({}, foo);
-// foo.a = 'dfghj';
-// console.log(foo);
-// console.log(fee);
+// const btns = document.querySelectorAll('.btn.btn-warning');
+// console.log(btns);
 
-// let fee = foo.pop();
-// console.log(fee);
-// console.log(foo);
+// btns.forEach(item => console.log(item));
 
-// const a = ['a', 'b', 'c'];
+const newButton = document.createElement('button');
+newButton.classList.add('btn', 'btn-outline-success');
+newButton.innerHTML = '<h1>Hello<h1>';
+// newButton.textContent = '<h2>Hello<h2>';
+// document.body.append(newButton);
 
-// a.forEach((item, index, arr) => {
-//     console.log(`${index} - value: ${item} - inside ${arr}`);
-// });
-
-// const personalMovieDB = {
-//     count: 0,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false,
-//     person: {
-//         name: 'Sviat',
-//         age: 19
-//     }
-// };
-
-// for (const key in personalMovieDB) {
-//     console.log(personalMovieDB[key]);
-// }
-
-// console.log(Object.keys(personalMovieDB.person).length);
-//  delete personalMovieDB.person;
-//  console.log(personalMovieDB);
+document.querySelector('.main').prepend(newButton);
